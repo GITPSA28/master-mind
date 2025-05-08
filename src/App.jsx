7736519272afffd7ab654ae9a4d40c977a5a35b8
Sunil <sunilaravindp@gmail.com>
2025-05-08 16:52:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <div className="streaks">18</div>
+        <div className="logo">
+          <p>Code Chase</p>
+        </div>
+        <div className="options-toggle">options</div>
+      </nav>
+    </>
+  );
+}
+
+export default App;
